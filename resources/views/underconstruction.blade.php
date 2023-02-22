05e4c07d8bcead005858f548a0d61b60b7e31417
anaifantri <ana.ifantri@gmail.com>
2023-02-22 16:40:46 +0800

    Install Tailwind, Menambah file underconstruction.blade.php, menambahkan folder img pada folder public

---
+++ resources/views/underconstruction.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>VISTA MEDIA | Under Construction</title>
+</head>
+
+<body>
+    <div class="container-xl mx-auto position-relative flex h-screen bg-fixed bg-center bg-auto"
+        style="background-image: url('https://source.unsplash.com/1366x768?nature')">
+        <div class="p-10 position-absolute m-auto">
+            <div
+                class="border border-transparent flex position-relative mb-5 mx-auto border-white border-opacity-50 rounded-full shadow shadow-white bg-white bg-opacity-20 w-32 h-32 md:w-40 lg:w-40 md:h-40 lg:h-40">
+                <img class="w-24 md:w-32 lg:w-32 m-auto position-absolute" src="/img/Logo-Vista-Media.png"
+                    alt="">
+            </div>
+            <h1
+                class="drop-shadow-lg shadow-black animate-pulse text-3xl md:text-5xl lg:text-5xl text-center text-white font-sans font-semibold tracking-wider">
+                Comming Soon</h1>
+            <p class="drop-shadow-md shadow-black text-white text-center position-absolute mt-3">our
+                website is
+                currently under construction</p>
+        </div>
+    </div>
+</body>
+
+</html>
